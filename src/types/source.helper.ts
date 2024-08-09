c3ea046c9456cb74623665b5d9d9217ac7b9692d
Carlos Alcala <solenasthellion@gmail.com>
2024-08-09 12:13:11 -0600

    feat: :sparkles: Add helper function to retrieve Source by ID
    Introduced a new file `source.helper.ts` containing utility functions  to work with source data.

---
+++ src/types/source.helper.ts
@@ -0,0 +1,23 @@
+import { Source, SourceID, SourceMap } from "./source.type";
+
+/**
+ * Retrieves a Source object by its ID.
+ *
+ * @param {SourceID} id - The unique identifier of the source.
+ * @returns {Source} The corresponding Source object with name and ID.
+ * @throws Will throw an error if the SourceID is invalid.
+ * @example const exampleSource = getSourceByID('PHB');
+ * console.log(exampleSource); // { id: 'PHB', name: "Player's Handbook" }
+ */
+export function getSourceByID(id: SourceID): Source {
+  const name = SourceMap[id];
+  
+  if (!name) {
+    throw new Error(`Invalid SourceID: ${id}`);
+  }
+
+  return {
+    id,
+    name,
+  };
+}
